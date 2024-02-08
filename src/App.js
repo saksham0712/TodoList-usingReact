705399ec757dde26acbb72913fde38c91ffa5df1
Saksham Verma <110185108+saksham0712@users.noreply.github.com>
2024-02-08 21:05:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+
+import './App.css';
+
+import Header from './My components/Header.js';
+import {ToDos} from "./My components/Todos.js";
+
+import {Footer} from "./My components/Footer.js";
+import {AddTodo} from "./My components/addTodo.js";
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+
+function App() {
+  let initTodo;
+  if(localStorage.getItem("todos")===null){
+initTodo=[]
+  }
+  else{
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+  
+  
+  const onDelete = (todo) => {
+    console.log('i am delete', todo)
+    setTodos(todos.filter((e)=>{
+      return e!==todo;
+    }))
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+const addTodo = (Title, desc) => {
+  let sno;
+  if(todos.length == 0){
+    sno = 0;
+  } else {
+    sno = todos[todos.length-1].sno+1;
+
+  }
+  console.log('i am addding the function',sno, Title, desc)
+  const myTodo = {
+    sno: sno,
+    title: Title,
+    desc: desc
+  }
+  setTodos([...todos, myTodo])
+  console.log(myTodo)
+
+}
+
+  const [todos, setTodos] = useState(initTodo)
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+  }, [todos])
+  return (
+    <>
+    
+      
+   <Header title = "ToDos " searchbar = {false}/>
+   <AddTodo addTodo={addTodo}/>
+   <ToDos todos = {todos} onDelete = {onDelete}/>
+   <Footer/>
+  
+
+
+
+    </>
+
+
+  );
+}
+
+export default App;
---
+++ src/My components/Todos.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import TodoItems from './TodoItems.js';
+
+export const ToDos = (props) => {
+
+  let myStyle = {
+    minHeight : "70vh"
+}
+  return (
+    <div className='container my-3' style={myStyle}>
+      <h3 className='my-3'>ToDos List</h3>
+      {props.todos.lenght===0 ? <h3>"No todos to show"</h3> :
+      props.todos.map((todo)=>{
+        return (
+          <>
+           <TodoItems todo={todo} key={todo.sno} onDelete={props.onDelete}/> <hr/>
+        </>
+        )
+      })
+    }
+      </div>
+  )
+}
+
